dc3c918983de242101d4d00b71e88947dfa6739e
GreatKittiphat00 <GreatKittiphat72@gmail.com>
2024-11-12 03:12:54 +0700

    Edit build file.

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    serverBuildTarget: "netlify",  // ใช้ Netlify ในการ build
-    appDirectory: "routes",           // ถ้าคุณใช้ src สำหรับโค้ดหลัก
+    serverBuildTarget: "netlify",
+    appDirectory: "app",
     publicPath: "/build/",
   };
   
